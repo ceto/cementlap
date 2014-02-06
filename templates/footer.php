603e162173cbe636db9ada7da073703ea8160416
Szabó Gábor <szabogabi@gmail.com>
2014-02-06 11:07:59 +0100

    first commit

---
+++ templates/footer.php
@@ -0,0 +1,13 @@
+<footer class="content-info" role="contentinfo">
+  <div class="footer-sidebar">
+    <section class="callnow">
+      <span class="duma">Kérjen ajánlatot!</span>
+      <a class="telcsi" href="tel:+36203174323">+36&nbsp;20<br>317 43&nbsp;23</a>
+    </section>
+    <?php dynamic_sidebar('sidebar-footer'); ?>
+  </div>
+  <div class="footer-copy">
+    © 2014 Mobilia Shops & Homes | Site by <a href="http://hydrogene.hu">HYDROGENE</a>
+  </div>
+</footer>
+<?php wp_footer(); ?>
