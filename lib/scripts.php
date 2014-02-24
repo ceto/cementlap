a20fc8486ed21d4acadc23c8a343749f89ed6eb2
Szabó Gábor <szabogabi@gmail.com>
2014-02-24 14:48:36 +0100

    product oldal footer

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '954284eb36ad8f644120449275101e9e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '934accec1554a30efae1f13d429e7a69');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
