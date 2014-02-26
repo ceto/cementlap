5106ca2105723189d4d5ccbc487fd9eef590fca0
Szabó Gábor <szabogabi@gmail.com>
2014-02-26 17:32:27 +0100

    minimal typography

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '83524051b96fc9a2ba2a86797a69bb9f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '87dcd84829872feb80bdc4a5415f4079');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
