16565cc00dcd41a45f91a86a25b195bc0722ec68
Szabó Gábor <szabogabi@gmail.com>
2014-03-06 10:34:59 +0100

    finomítás blogview

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd6f0e9a53a55237c330e908e46e6aa59');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'cb16785974a5b42602c29742923d6c6f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
