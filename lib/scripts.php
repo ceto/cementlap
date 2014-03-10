99a3d55ad84f6b6f64e89edaf5ec9ba1b3a5d59d
Szabó Gábor <szabogabi@gmail.com>
2014-03-10 12:55:39 +0100

    page margin

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b0cc053c4d7a5d74402c45f254db5328');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '24d8cc27dbdc3ca3c88a4c9b9c535429');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
