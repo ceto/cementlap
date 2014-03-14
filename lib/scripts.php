7bc661754abe8854bc5853564459ab35668339cb
Szabó Gábor <szabogabi@gmail.com>
2014-03-14 18:04:38 +0100

    font

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '571c63d11004e091eb98d81bdd963ae8');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c6289975b990a9f3a0a403b31bbe0c01');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
