d936f242269c645c857f6ea7fd6435df4356ada8
Szabó Gábor <szabogabi@gmail.com>
2014-03-31 12:05:33 +0200

    combi webfont fix

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '684303ff5242ecd746c422f75aa3ac26');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f81ad2d0d86be232264dd20f911aa38a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
