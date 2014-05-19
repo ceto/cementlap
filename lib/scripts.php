86d2cdedb14c56d54a866276f93473be6ac65d89
Szabó Gábor <szabogabi@gmail.com>
2014-05-19 12:43:55 +0200

    footercolor

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '50c47e9c3dc5b89046db4b9ce4b416dc');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '24a11453d1ea122b8770ddbcb0205782');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/subnavigation.php
+++ templates/subnavigation.php
@@ -1,13 +1,13 @@
 <?php if (is_page() && !is_front_page() && ($post->post_parent!=0) ): ?>
 <nav class="nav-sub" role="navigation">
-  <h2 class="subnav-title">Segítség</h2>
+  <!--h2 class="subnav-title">Segítség</h2-->
   <?php if (has_nav_menu('vasinfo_navigation')) : ?>
     <h3><?php _e('Vásárlási információk','root') ?></h3>    
    <?php wp_nav_menu(array('theme_location' => 'vasinfo_navigation', 'menu_class' => 'nav-vasinfo nav nav-pills')); ?>
   <?php endif; ?>
 
   <?php if (has_nav_menu('lerakinfo_navigation')) : ?>
-    <h3><?php _e('Lerakás és minőség','root') ?></h3>    
+    <h3><?php _e('Termék információ','root') ?></h3>    
     <?php wp_nav_menu(array('theme_location' => 'lerakinfo_navigation', 'menu_class' => 'nav-lerakinfo nav nav-pills')); ?>
   <?php endif; ?>
 
