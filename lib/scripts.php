22e84ddda8281bc33b976682dd1c1ef00fa32e2d
Szabó Gábor <szabogabi@gmail.com>
2014-06-03 17:26:34 +0200

    forditashoz

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '23323484359e3baca81afa726d751766');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6f37d4e6ba7ee1fd1af1858f5f9ff27f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
