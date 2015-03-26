bb43bca2514604588003edc2a4b5ffb65f21baf9
Gabor Szabo <szabogabi@gmail.com>
2015-03-26 13:39:01 +0100

    logo pl jav3

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b765bfe2ea2555935b2c14fe383e38c9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c956f5254a8e0c6bbbea011fe0e4ba6e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
