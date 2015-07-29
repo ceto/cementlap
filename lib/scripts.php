d8495cfb75be3960824bc100435ee0ea14ae21b5
Gabor Szabo <szabogabi@gmail.com>
2015-07-29 12:40:31 +0200

    inverted menuclass [deploy: live]

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c956f5254a8e0c6bbbea011fe0e4ba6e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7799271bd64fe2e0ba97a92c7ddbe854');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
