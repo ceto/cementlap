56e3a2cc852997274f54d40438c2addb8e975ddf
Gabor Szabo <szabogabi@gmail.com>
2015-09-07 12:50:28 +0200

    Nyelv [Deploy: Live]

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3581112fb4fea1ad547dabc0ff839dec');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5663783679e4829431189a80ab177f8c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
