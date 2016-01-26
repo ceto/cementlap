58be97e32eb187122273a86a5e92b540a0cdc170
Gabor Szabo <szabogabi@gmail.com>
2016-01-26 12:07:46 +0100

    widepage template [Deploy: Live]

--- templates/subnavigation.php
+++ templates/subnavigation.php
@@ -1,7 +1,6 @@
 <?php if (is_page() && !is_front_page() && !is_page_template('tmpl-widepage.php') && ($post->post_parent!=0) ): ?>
 <nav class="nav-sub" role="navigation">
   <h2 class="subnav-title"><?php  _e('Information','cementlap') ?></h2>
-
   <div class="snavitem">
   <?php if (has_nav_menu('vasinfo_navigation')) : ?>
     <h3><?php _e('Buyers\' information','cementlap') ?></h3>
@@ -22,6 +21,5 @@
     <?php wp_nav_menu(array('theme_location' => 'kapcsinfo_navigation', 'menu_class' => 'nav-kapcsinfo nav nav-pills')); ?>
   <?php endif; ?>
   </div>
-
 </nav>
 <?php endif; ?>
