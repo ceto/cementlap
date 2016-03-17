a4bbf41057eb1880115c3a124f9b4c4855547a60
Gabor Szabo <szabogabi@gmail.com>
2016-03-17 15:48:44 +0100

    [Deploy: Live]

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ff4abb448cef44ae4d19ba01dccf8511');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '754d6333d419c1de937023b775289c62');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
