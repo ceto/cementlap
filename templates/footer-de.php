7e08b267e64abaf8c61f11c8a7045186d40cfcc7
Gabor Szabo <szabogabi@gmail.com>
2016-08-01 22:04:53 +0200

    de face added [Deploy: Live]

--- templates/footer-de.php
+++ templates/footer-de.php
@@ -1,6 +1,19 @@
+<style>
+.footer-copy {
+  float:left;
+}
+.footer-right {
+  float:right;
+}
+</style>
 <footer class="content-info" role="contentinfo">
   <div class="footer-copy">
     © 2016 Marrakesh Zementfliesen | Design & Site by <a href="http://hydrogene.hu/referencia/termek-bongeszo-es-foto-galeria/">HYDROGENE</a>
+  </div>
+  <div class="footer-right">
+    <a href="https://www.facebook.com/MarrakeshLakberendezes" class="socialgomb" target="_blank">
+      <i class="ion ion-social-facebook"></i>
+    </a>
   </div>
   <a href="#" class="tothetop"><i class="ion-ios-arrow-thin-up"></i></a>
 </footer>
