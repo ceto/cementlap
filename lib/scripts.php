b0c1459ba4296172771d92575f35214dec609fc3
Gabor Szabo <szabogabi@gmail.com>
2016-10-17 12:40:52 +0200

    fr nl logo added [Deploy: Live]

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0a8e051bb351c24f89509598459f3463');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '83a422db2f3bd5455d4a079d6f0732c4');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
