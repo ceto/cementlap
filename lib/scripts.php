de48e60eac8fcf66d6f7ad6e2913d759791522b5
Szabó Gábor <szabogabor@hydrogene.hu>
2017-05-22 11:48:35 +0200

    at logo elmaszott

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '460a48d42e37ce65606f2b4b7e2fc4fe');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'adcbd77bf3cb3544a68979990508c4d9');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
