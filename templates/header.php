b454c7b0a63b9fac6dcfca0bbbcbbca5bb46aaed
Szabó Gábor <szabogabor@hydrogene.hu>
2017-08-11 01:22:17 +0200

    osztrák telszam fix

--- templates/header.php
+++ templates/header.php
@@ -16,7 +16,7 @@
       <?php
         switch (ICL_LANGUAGE_CODE) {
           case 'de': ?>
-            <li class="tel"><a href="tel:003623950282"><i class="ion ion-iphone"></i> (+43) 660 822 2329</a></li>
+            <li class="tel"><a href="tel:00436608222329"><i class="ion ion-iphone"></i> (+43) 660 822 2329</a></li>
             <li class="email"><a href="mailto:info@marrakeshzementfliesen.at"><i class="ion ion-email"></i> info@marrakeshzementfliesen.at</a></li>
           <? break;
           /* case 'fr': ?>
