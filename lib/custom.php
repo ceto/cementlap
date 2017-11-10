55eb68d64d86ebd888d1a884360246bcd2eca2e3
Szabó Gábor <szabogabi@gmail.com>
2017-11-10 12:18:13 +0100

    bower comp delete

--- lib/custom.php
+++ lib/custom.php
@@ -25,12 +25,12 @@ function cement_acf_settings_dir( $dir ) {
   return $dir;
 }
 // 3. Hide ACF field group menu item
-//add_filter('acf/settings/show_admin', '__return_false');
+add_filter('acf/settings/show_admin', '__return_false');
 
 // 4. Include ACF
 include_once( get_stylesheet_directory() . '/lib/acf/acf.php' );
 
-//include_once( get_stylesheet_directory() . '/lib/cement-acf.php' );
+include_once( get_stylesheet_directory() . '/lib/cement-acf.php' );
 
 
 
