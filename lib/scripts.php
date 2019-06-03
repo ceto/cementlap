6b48cb1c6e93bcba02628d94c08fef63408f87b6
Gabor Szabo <szabogabi@gmail.com>
2019-06-03 20:31:33 +0200

    js added owl error

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '111eae8437179ab5b399d4493fc343e9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '534108d999ca4d4900eb38f963fafb45');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
